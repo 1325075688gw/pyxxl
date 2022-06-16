3878fd8c8df47e343b7d93bd40c9aab3d647373d
tony Stark <tony@test.com>
2022-06-16 20:37:33 +0800

    env

--- peach/env.py
+++ peach/env.py
@@ -1,10 +1,11 @@
-import imp
 import os
 import yaml
+from yaml import Loader
 
 _env_file = open(os.path.join(os.getcwd(), "env.yaml"), "r")
-_env = yaml.load(_env_file.readlines())
+_env = yaml.load(_env_file, Loader)
 _env_file.close()
+
 
 def get_env(name: str, default) -> str:
     return _env.get(name, default)
