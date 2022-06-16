6780aa7d82f816a3a52a5ec1a21cb02e55d43075
tony Stark <tony@test.com>
2022-06-16 20:55:23 +0800

    env

--- peach/env.py
+++ peach/env.py
@@ -1,11 +1,12 @@
+"""
+
+从工程根目录下读取 env.yaml 文件内容并解析成 ENV 变量。
+
+"""
 import os
 import yaml
 from yaml import Loader
 
 _env_file = open(os.path.join(os.getcwd(), "env.yaml"), "r")
-_env = yaml.load(_env_file, Loader)
+ENV = yaml.load(_env_file, Loader)
 _env_file.close()
-
-
-def get_env(name: str, default=None) -> str:
-    return _env.get(name, default)
