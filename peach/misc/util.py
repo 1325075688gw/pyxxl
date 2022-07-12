9ffdf92402a185ef42d28dba4405d8091a645c1a
jaeger <jaeger1111@outlook.com>
2022-07-12 10:32:12 +0000

    feat: 增加timer组件

--- peach/misc/util.py
+++ peach/misc/util.py
@@ -7,7 +7,7 @@ from dacite import from_dict, Config
 from peach.misc import dt
 
 
-def dict_to_dto(msg: typing.Dict, data_class: Type) -> Any:
+def dict_to_dto(data: typing.Dict, data_class: Type) -> Any:
     return from_dict(
         data_class=data_class,
         data=data,
