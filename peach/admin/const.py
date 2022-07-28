6bd4c286a8f8cca88ea24473fd2d23321531d54b
Tom <tom@binarywalk.com>
2022-07-28 08:49:40 +0000

    Update const.py

--- peach/admin/const.py
+++ peach/admin/const.py
@@ -3,4 +3,4 @@ SESSION_USER_KEY = "user_id"
 ACTION = {"POST": 1, "PUT": 2, "DELETE": 3, "GET": 4}
 ActionDesc = {1: "创建", 2: "编辑", 3: "删除", 4: "导出"}
 
-DEFAULT_PASSWORD = "123!@#qwe"
+DEFAULT_PASSWORD = "123!@#Qwe"
