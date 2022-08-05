567dbe912f4e2cb6834a682d9e607e70e5170148
Jaeger <JaegerCode@gmail.com>
2022-08-05 11:01:10 +0800

    feat: 优化db_encrypt

--- peach/misc/db_encrypt.py
+++ peach/misc/db_encrypt.py
@@ -7,9 +7,13 @@ _AES = AESCipher(base64.b64decode(settings.DB_AES_KEY))
 
 def db_encrypt(value: str) -> str:
     """DB数据加密"""
+    if not value:
+        return ""
     return base64.b64encode(_AES.encrypt(value.encode())).decode()
 
 
 def db_decrypt(value: str) -> str:
     """DB数据解密"""
+    if not value:
+        return ""
     return _AES.decrypt(base64.b64decode(value)).decode()
