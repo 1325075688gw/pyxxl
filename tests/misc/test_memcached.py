a4539d9756f03d234a1f585fd7fb05783c9f5793
Bruce <brucex@binarywalk.com>
2022-11-03 13:38:47 +0800

    fix link error

--- tests/misc/test_memcached.py
+++ tests/misc/test_memcached.py
@@ -1,11 +1,12 @@
-
 import time
 from peach.misc import memcached
 
-def test_memcached():
-    _is_fetch = type("store", (object,), {'value': False})
 
-    _test_data = [1,2,3]
+def test_memcached():
+    _is_fetch = type("store", (object,), {"value": False})
+
+    _test_data = [1, 2, 3]
+
     def _fetch_func():
         _is_fetch.value = True
         return _test_data
@@ -13,16 +14,16 @@ def test_memcached():
     def _query():
         _is_fetch.value = False
         return memcached.get("test_key", fetch_func=_fetch_func, expire_sec=5)
-    
-    data = _query()
-    assert data == _test_data
-    assert _is_fetch.value == True
 
     data = _query()
     assert data == _test_data
-    assert _is_fetch.value == False
+    assert _is_fetch.value is True
+
+    data = _query()
+    assert data == _test_data
+    assert _is_fetch.value is False
 
     time.sleep(6)
     data = _query()
     assert data == _test_data
-    assert _is_fetch.value == True
+    assert _is_fetch.value is True
