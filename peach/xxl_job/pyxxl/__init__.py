5278a95ee152ce1e5ce224aa299c9a24dda8c1db
gavin <gavin.g@binarywalk.com>
2023-02-02 14:33:55 +0800

    flake8

--- peach/xxl_job/pyxxl/__init__.py
+++ peach/xxl_job/pyxxl/__init__.py
@@ -1,8 +1,16 @@
-# import importlib.metadata
-#
-# from .executor import JobHandler
-# from .main import PyxxlRunner
-# from .setting import ExecutorConfig
+import importlib.metadata
+
+from .executor import JobHandler
+from .main import PyxxlRunner
+from .setting import ExecutorConfig
+
+
+def help():
+    m = importlib.metadata
+    j = JobHandler
+    p = PyxxlRunner
+    e = ExecutorConfig
+    return m, j, p, e
 
 
 # __version__ = importlib.metadata.version("pyxxl")
